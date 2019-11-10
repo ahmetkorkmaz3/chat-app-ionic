e8c28416ecd64fae59df76965dd5a2114d5965f4
ahmetkorkmaz3 <ahmetkorkmaz3453@gmail.com>
2019-11-10 19:01:43 +0300

    chat room tasarımı düzenlendi

--- src/app/chat-room/chat-room.page.ts
+++ src/app/chat-room/chat-room.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-chat-room',
@@ -10,10 +11,44 @@ export class ChatRoomPage implements OnInit {
 
   username: string;
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
+  algorithm: string;
+  message: string;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
     this.username = this.activatedRoute.snapshot.paramMap.get('username');
+  }
+
+  sendMessage() {
+    if (this.message && this.algorithm) {
+      const data = {
+        message: this.message,
+        algorithm: this.algorithm
+      };
+    } else {
+      const errorData = {
+        header: 'Hata!',
+        subHeader: 'Eksik argüman',
+        message: 'Mesaj girmediniz veya algoritma seçimi yapmadınız.',
+        buttons: 'Tamam'
+      };
+      this.alert(errorData);
+    }
+  }
+
+  async alert(alertData) {
+    const alert = await this.alertController.create({
+      header: alertData.header,
+      subHeader: alertData.subHeader,
+      message: alertData.message,
+      buttons: [alertData.buttons]
+    });
+    await alert.present();
   }
 
 }
