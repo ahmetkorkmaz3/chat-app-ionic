440fecd6c676ab2e1ed5e605c501e2cdd1b2e4f1
ahmetkorkmaz3 <ahmetkorkmaz3453@gmail.com>
2019-11-12 02:54:47 +0300

    vigenere algoritması eklendi

--- src/app/chat-room/chat-room.page.ts
+++ src/app/chat-room/chat-room.page.ts
@@ -20,6 +20,8 @@ export class ChatRoomPage implements OnInit {
 
   isUserMe: boolean;
 
+  vigenereKey = 'abc';
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -77,7 +79,99 @@ export class ChatRoomPage implements OnInit {
   subscribeToChat() {
     this.socket.fromEvent('chat').subscribe(data => {
       this.messages.push(data);
+      console.log(data);
     });
   }
+
+  /* Vigenere Crypt */
+
+  // tslint:disable-next-line: member-ordering
+  tabulaRecta = {
+    a: 'abcdefghijklmnopqrstuvwxyz',
+    b: 'bcdefghijklmnopqrstuvwxyza',
+    c: 'cdefghijklmnopqrstuvwxyzab',
+    d: 'defghijklmnopqrstuvwxyzabc',
+    e: 'efghijklmnopqrstuvwxyzabcd',
+    f: 'fghijklmnopqrstuvwxyzabcde',
+    g: 'ghijklmnopqrstuvwxyzabcdef',
+    h: 'hijklmnopqrstuvwxyzabcdefg',
+    i: 'ijklmnopqrstuvwxyzabcdefgh',
+    j: 'jklmnopqrstuvwxyzabcdefghi',
+    k: 'klmnopqrstuvwxyzabcdefghij',
+    l: 'lmnopqrstuvwxyzabcdefghijk',
+    m: 'mnopqrstuvwxyzabcdefghijkl',
+    n: 'nopqrstuvwxyzabcdefghijklm',
+    o: 'opqrstuvwxyzabcdefghijklmn',
+    p: 'pqrstuvwxyzabcdefghijklmno',
+    q: 'qrstuvwxyzabcdefghijklmnop',
+    r: 'rstuvwxyzabcdefghijklmnopq',
+    s: 'stuvwxyzabcdefghijklmnopqr',
+    t: 'tuvwxyzabcdefghijklmnopqrs',
+    u: 'uvwxyzabcdefghijklmnopqrst',
+    v: 'vwxyzabcdefghijklmnopqrstu',
+    w: 'wxyzabcdefghijklmnopqrstuv',
+    x: 'xyzabcdefghijklmnopqrstuvw',
+    y: 'yzabcdefghijklmnopqrstuvwx',
+    z: 'zabcdefghijklmnopqrstuvwxy'
+  };
+
+  vigenereEncrypt(plainText, keyword) {
+    if ( typeof(plainText) !== 'string' ) {
+      return 'invalid plainText. Must be string, not ' + typeof(plainText);
+    }
+    if ( typeof(keyword) !== 'string' ) {
+      return 'invalid keyword. Must be string, not ' + typeof(keyword);
+    }
+
+    plainText = plainText.toLowerCase();
+    keyword = keyword.match(/[a-z]/gi).join('').toLowerCase();
+    let encryptedText = '';
+    let specialCharacterCount = 0;
+
+    for ( let i = 0; i < plainText.length; i++ ) {
+      const keyLetter = (i - specialCharacterCount) % keyword.length;
+      const keywordIndex = this.tabulaRecta.a.indexOf(keyword[keyLetter]);
+
+      if ( this.tabulaRecta[plainText[i]] ) {
+        encryptedText += this.tabulaRecta[plainText[i]][keywordIndex];
+      } else {
+        encryptedText += plainText[i];
+        specialCharacterCount++;
+      }
+    }
+
+    return encryptedText;
+  }
+
+  vigenereDecrypt(encryptedText, keyword) {
+    if ( typeof(encryptedText) !== 'string' ) {
+      return 'invalid encryptedText. Must be string, not ' + typeof(encryptedText);
+    }
+    if ( typeof(keyword) !== 'string' ) {
+      return 'invalid keyword. Must be string, not ' + typeof(keyword);
+    }
+
+    encryptedText = encryptedText.toLowerCase();
+    keyword = keyword.match(/[a-z]/gi).join('').toLowerCase();
+    let decryptedText = '';
+    let specialCharacterCount = 0;
+
+    for ( let i = 0; i < encryptedText.length; i++ ) {
+      const keyLetter = (i - specialCharacterCount) % keyword.length;
+      const keyRow = this.tabulaRecta[keyword[keyLetter]];
+
+      if ( keyRow.indexOf(encryptedText[i]) !== -1 ) {
+        decryptedText += this.tabulaRecta.a[keyRow.indexOf(encryptedText[i])];
+      } else {
+        decryptedText += encryptedText[i];
+        specialCharacterCount++;
+      }
+    }
+
+    return decryptedText;
+  }
+
+  /* Vigenere Crypt End */
+
 
 }
