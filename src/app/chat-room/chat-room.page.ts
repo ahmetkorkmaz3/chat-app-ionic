6a9048940b89a2a28e4cf26a75a3e9cc69bc0e11
ahmetkorkmaz3 <ahmetkorkmaz3453@gmail.com>
2019-12-22 14:01:43 +0300

    Şifreleme algoritmaları eklendi

--- src/app/chat-room/chat-room.page.ts
+++ src/app/chat-room/chat-room.page.ts
@@ -41,6 +41,33 @@ export class ChatRoomPage implements OnInit {
 
   sendMessage() {
     if (this.messageText && this.algorithm) {
+
+      switch (this.algorithm) {
+        case "sezar": {
+          this.messageText = this.ceasar_encrypt(this.messageText, { key: 30 });
+          break;
+        }
+        case "vigenere": {
+          this.messageText = this.vigenereEncrypt(this.messageText, this.vigenereKey);
+          break;
+        }
+        case "polybius": {
+          this.messageText = this.polybius_encrypt(this.messageText);
+          break;
+        }
+        case "picket": {
+          this.messageText = this.picket_fence_encrypt(this.messageText);
+          break;
+        }
+        case "columnar": {
+          this.messageText = this.columnar_encrypt(this.messageText);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+
       const data = {
         username: this.username,
         message: this.messageText,
@@ -48,6 +75,7 @@ export class ChatRoomPage implements OnInit {
       };
 
       this.socket.emit('chat', data);
+      this.messageText = "";
 
     } else {
       const errorData = {
@@ -77,13 +105,39 @@ export class ChatRoomPage implements OnInit {
   }
 
   subscribeToChat() {
-    this.socket.fromEvent('chat').subscribe(data => {
+    this.socket.fromEvent('chat').subscribe((data: any) => {
+      console.log(data);
+      switch (data.algorithm) {
+        case "sezar": {
+          data.message = this.ceasar_decrypt(data.message, { key: 30 });
+          break;
+        }
+        case "vigenere": {
+          data.message = this.vigenereDecrypt(data.message, this.vigenereKey);
+          break;
+        }
+        case "polybius": {
+          data.message = this.polybius_decrypt(data.message);
+          break;
+        }
+        case "picket": {
+          data.message = this.picket_fence_decrypt(data.message);
+          break;
+        }
+        case "columnar": {
+          data.message = this.columnar_decrypt(data.message);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+
       this.messages.push(data);
       console.log(data);
     });
   }
 
-  /* Vigenere Crypt */
 
   // tslint:disable-next-line: member-ordering
   tabulaRecta = {
@@ -115,12 +169,32 @@ export class ChatRoomPage implements OnInit {
     z: 'zabcdefghijklmnopqrstuvwxy'
   };
 
+
+  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ",", "?", "x", "w", "q"];
+
+  alphabet_matris = [
+    ["a", "b", "c", "d", "e", "f", "g"],
+    ["h", "i", "j", "k", "l", "m", "n"],
+    ["o", "p", "r", "s", "t", "u", "v"],
+    ["y", "z", "1", "2", "3", "4", "5"],
+    ["6", "7", "8", "9", "0", " ", "."],
+    [",", "?", "w", "q", "x"]
+  ];
+
+  transpose(a) {
+    return Object.keys(a[0]).map(function (c) {
+      return a.map(function (r) { if (r[c]) { return r[c] } else { return "" }; });
+    });
+  }
+
+  /* Vigenere Crypt */
+
   vigenereEncrypt(plainText, keyword) {
-    if ( typeof(plainText) !== 'string' ) {
-      return 'invalid plainText. Must be string, not ' + typeof(plainText);
+    if (typeof (plainText) !== 'string') {
+      return 'invalid plainText. Must be string, not ' + typeof (plainText);
     }
-    if ( typeof(keyword) !== 'string' ) {
-      return 'invalid keyword. Must be string, not ' + typeof(keyword);
+    if (typeof (keyword) !== 'string') {
+      return 'invalid keyword. Must be string, not ' + typeof (keyword);
     }
 
     plainText = plainText.toLowerCase();
@@ -128,11 +202,11 @@ export class ChatRoomPage implements OnInit {
     let encryptedText = '';
     let specialCharacterCount = 0;
 
-    for ( let i = 0; i < plainText.length; i++ ) {
+    for (let i = 0; i < plainText.length; i++) {
       const keyLetter = (i - specialCharacterCount) % keyword.length;
       const keywordIndex = this.tabulaRecta.a.indexOf(keyword[keyLetter]);
 
-      if ( this.tabulaRecta[plainText[i]] ) {
+      if (this.tabulaRecta[plainText[i]]) {
         encryptedText += this.tabulaRecta[plainText[i]][keywordIndex];
       } else {
         encryptedText += plainText[i];
@@ -144,11 +218,11 @@ export class ChatRoomPage implements OnInit {
   }
 
   vigenereDecrypt(encryptedText, keyword) {
-    if ( typeof(encryptedText) !== 'string' ) {
-      return 'invalid encryptedText. Must be string, not ' + typeof(encryptedText);
+    if (typeof (encryptedText) !== 'string') {
+      return 'invalid encryptedText. Must be string, not ' + typeof (encryptedText);
     }
-    if ( typeof(keyword) !== 'string' ) {
-      return 'invalid keyword. Must be string, not ' + typeof(keyword);
+    if (typeof (keyword) !== 'string') {
+      return 'invalid keyword. Must be string, not ' + typeof (keyword);
     }
 
     encryptedText = encryptedText.toLowerCase();
@@ -156,11 +230,11 @@ export class ChatRoomPage implements OnInit {
     let decryptedText = '';
     let specialCharacterCount = 0;
 
-    for ( let i = 0; i < encryptedText.length; i++ ) {
+    for (let i = 0; i < encryptedText.length; i++) {
       const keyLetter = (i - specialCharacterCount) % keyword.length;
       const keyRow = this.tabulaRecta[keyword[keyLetter]];
 
-      if ( keyRow.indexOf(encryptedText[i]) !== -1 ) {
+      if (keyRow.indexOf(encryptedText[i]) !== -1) {
         decryptedText += this.tabulaRecta.a[keyRow.indexOf(encryptedText[i])];
       } else {
         decryptedText += encryptedText[i];
@@ -172,6 +246,157 @@ export class ChatRoomPage implements OnInit {
   }
 
   /* Vigenere Crypt End */
+
+  /* Ceaser Crypt Start */
+
+  // data.key = ötleme sayısı
+  ceasar_encrypt(message, data) {
+    let response = "";
+    const long_alphabet = [...this.alphabet, ...this.alphabet];
+    [...message].forEach(char => {
+      char = char.toLowerCase();
+      let index = this.alphabet.findIndex(item => item == char);
+      if (index >= 0) {
+        response += long_alphabet[index + data.key];
+      }
+    });
+    return response;
+  }
+
+  ceasar_decrypt(text, data) {
+    let response = "";
+    [...text].forEach(char => {
+      let index = this.alphabet.findIndex(item => item == char);
+      if (index >= 0) {
+        if (index - data.key >= 0) {
+          response += this.alphabet[index - data.key];
+        } else {
+          response += this.alphabet[(index - data.key) + this.alphabet.length];
+        }
+      }
+    });
+    return response;
+  }
+
+  /* Ceaser Crypt End */
+
+  /* Polybius Crypt Start */
+
+  polybius_encrypt(message) {
+    let response = "";
+    [...message].forEach(char => {
+      let row_index;
+      let col_index;
+      for (let i = 0; i < this.alphabet_matris.length; i++) {
+        const temp_alphabet = this.alphabet_matris[i];
+        let temp_index = temp_alphabet.findIndex(item => item == char);
+        if (temp_index >= 0) {
+          col_index = i;
+          row_index = temp_index;
+        }
+      }
+      if (row_index >= 0 && col_index >= 0) {
+        response += col_index + "" + row_index;
+      }
+    });
+    return response;
+  }
+
+  polybius_decrypt(text) {
+    let response = "";
+    let key_wait = true;
+    let temp_col;
+    [...text].forEach(char => {
+      if (key_wait == true) {
+        temp_col = char;
+        key_wait = false;
+      } else {
+        response += this.alphabet_matris[parseInt(temp_col)][parseInt(char)];
+        temp_col = null;
+        key_wait = true;
+      }
+    });
+    return response;
+  }
+
+  /* Polybius Crypt End */
+
+  /* Picket Fence Crypt Start */
+  picket_fence_encrypt(message) {
+    let single = "";
+    let double = "";
+    [...message].forEach((char, index) => {
+      if (index % 2 == 0) {
+        double += char;
+      } else {
+        single += char;
+      }
+    });
+    return double + single;
+  }
+
+  picket_fence_decrypt(text) {
+    let response = "";
+    let middle;
+    if (text.length % 2 == 0) {
+      middle = text.length / 2;
+    } else {
+      middle = (text.length + 1) / 2;
+    }
+    let start = true;
+    let buffer = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (start) {
+        response += text[buffer];
+        start = false;
+      } else {
+        response += text[buffer + middle];
+        buffer++;
+        start = true;
+      }
+    }
+    return response;
+  }
+
+  /* Picket Fence Crypt End */
+
+  /* Columnar Crypt Start */
+
+  columnar_encrypt(message) {
+    let response = "";
+    let matris = [];
+    let square = Math.sqrt(message.length);
+    let size = Math.ceil(square);
+    for (let i = 0; i < size; i++) {
+      matris.push(message.substr(size * i, size));
+    }
+    let new_matris = this.transpose(matris);
+    [...new_matris].forEach(item => {
+      let word = "";
+      [...item].forEach(char => {
+        word += char;
+      });
+      response += word + "$";
+    });
+    return response;
+  }
+
+  columnar_decrypt(text) {
+    let response = "";
+    let matris = text.split("$");
+    let new_matris = this.transpose(matris);
+    [...new_matris].forEach(item => {
+      let word = "";
+      [...item].forEach(char => {
+        word += char;
+      });
+      response += word
+    });
+    return response;
+  }
+
+  /* Columnar Crypt End */
+
 
 
 }
